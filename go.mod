36a997b11cccea0e94ca442e4929845fd663cb99
Meng-Ying Yang <ymy1019@gmail.com>
2021-04-25 22:14:29 +0800

    im: slack: add slack event parsing and process

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.7.1
 	github.com/google/subcommands v1.2.0
 	github.com/larksuite/oapi-sdk-go v1.1.27
+	github.com/slack-go/slack v0.9.0 // indirect
 )
---
+++ im/slack/slack.go
@@ -0,0 +1,143 @@
+package slack
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+
+	botCmd "github.com/myyang/imbot/commands"
+	botLog "github.com/myyang/imbot/log"
+)
+
+var (
+	once      sync.Once
+	mutex     sync.Mutex
+	singleton *Slack
+)
+
+const (
+	slackPath = "/slack"
+)
+
+type Slack struct {
+	api           *slack.Client
+	signingSecret string
+}
+
+// RegisterGin once with pre-defined path
+func RegisterGin(r *gin.Engine) {
+	r.POST(slackPath, New().handleHttp)
+}
+
+// New returns slack bot instance.
+func New() *Slack {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if singleton != nil {
+		return singleton
+	}
+
+	singleton = &Slack{
+		api:           slack.New(os.Getenv("SLACK_TOKEN")),
+		signingSecret: os.Getenv("SLACK_SIGNING_SECRET"),
+	}
+	return singleton
+}
+
+func (s *Slack) handleHttp(c *gin.Context) {
+	raw, err := c.GetRawData()
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	secretVerifier, err := slack.NewSecretsVerifier(c.Request.Header, s.signingSecret)
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	_, err = secretVerifier.Write(raw)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	err = secretVerifier.Ensure()
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	eventsAPIEvent, err := slackevents.ParseEvent(
+		json.RawMessage(raw),
+		slackevents.OptionNoVerifyToken(),
+	)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	switch eventsAPIEvent.Type {
+	case slackevents.URLVerification:
+		var r *slackevents.ChallengeResponse
+		err := json.Unmarshal(raw, &r)
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		c.String(http.StatusOK, r.Challenge)
+	case slackevents.CallbackEvent:
+		innerEvent := eventsAPIEvent.InnerEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			opt := map[string]interface{}{
+				"channel_id": ev.Channel,
+			}
+			ctx := context.WithValue(
+				context.Background(),
+				botLog.CtxLogger,
+				s.Logger(opt),
+			)
+			botCmd.Execute(ctx, strings.Split(strings.Trim(ev.Text, " "), " "))
+		}
+
+		c.Status(http.StatusOK)
+	}
+}
+
+func (s *Slack) Logger(opt map[string]interface{}) (l *SlackLogger) {
+	l = &SlackLogger{
+		s:   s,
+		opt: opt,
+	}
+	return
+}
+
+type SlackLogger struct {
+	s   *Slack
+	opt map[string]interface{}
+}
+
+func (s *SlackLogger) Debugf(tmpl string, args ...interface{}) {
+	s.s.api.PostMessage(
+		os.Getenv("SLACK_LOG_CHANNEL"),
+		slack.MsgOptionText(fmt.Sprintf(tmpl, args...), false),
+	)
+}
+
+func (s *SlackLogger) Infof(tmpl string, args ...interface{}) {
+	s.s.api.PostMessage(
+		s.opt["channel_id"].(string),
+		slack.MsgOptionText(fmt.Sprintf(tmpl, args...), false),
+	)
+}
