80dd98dc0f8c497bf416d63ef125f7fd0b8b9619
Meng-Ying Yang <ymy1019@gmail.com>
2021-05-23 21:23:20 +0800

    log: add naive Logger interface for IM tool
    Add naive Logger interface, which embedded io.Writer, for IM tool to
    align IM tools for bot commands.

--- log/logger.go
+++ log/logger.go
@@ -1,5 +1,15 @@
 package log
 
+import "io"
+
 const (
 	CtxLogger = "_ctx_logger_"
 )
+
+// Logger is shared interface for imbot internally.
+type Logger interface {
+	io.Writer
+
+	Debugf(msg string, args ...interface{})
+	Infof(msg string, args ...interface{})
+}
