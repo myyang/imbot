cee7914d67e27d24ec230500c9de82701b05ae02
Meng-Ying Yang <ymy1019@gmail.com>
2021-07-06 17:17:34 +0800

    log: add DebugLogger struct
    For easy debugging with console, this DebugLogger implements log.Logger
    and proxies fmt lib for developers.

--- log/logger.go
+++ log/logger.go
@@ -1,6 +1,9 @@
 package log
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // CtxLogger as context key.
 var CtxLogger struct{}
@@ -11,4 +14,20 @@ type Logger interface {
 
 	Debugf(msg string, args ...interface{})
 	Infof(msg string, args ...interface{})
+}
+
+// DebugLogger implements Logger and proxies fmt lib for debugging.
+type DebugLogger struct{}
+
+func (l *DebugLogger) Debugf(msg string, args ...interface{}) {
+	fmt.Printf("[debug] "+msg, args...)
+}
+
+func (l *DebugLogger) Infof(msg string, args ...interface{}) {
+	fmt.Printf("[info] "+msg, args...)
+}
+
+func (l *DebugLogger) Write(p []byte) (n int, err error) {
+	fmt.Printf("[write] %s", p)
+	return 0, nil
 }
