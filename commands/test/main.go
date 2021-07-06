f10b4f3b88fdf82c546b994eacf96b8f04d5b9fb
Meng-Ying Yang <ymy1019@gmail.com>
2021-07-06 17:23:45 +0800

    commands: test: integrate DebugLogger to test main

--- commands/test/main.go
+++ commands/test/main.go
@@ -5,10 +5,12 @@ import (
 	"os"
 
 	botCmd "github.com/myyang/imbot/commands"
+	botLog "github.com/myyang/imbot/log"
 )
 
 func main() {
 	ctx := context.Background()
+	ctx = context.WithValue(ctx, botLog.CtxLogger, &botLog.DebugLogger{})
 
 	botCmd.Execute(ctx, os.Args[1:])
 }
