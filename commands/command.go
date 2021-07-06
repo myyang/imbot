3e5a44ad51444e8c3f71e34b3fe79eff449c6f71
Meng-Ying Yang <ymy1019@gmail.com>
2021-07-06 17:21:36 +0800

    commands: add root command to list/aggregate commands
    For more friendly usage to find and list commands, add root command to
    aggregate custom commands and list usage of all commands.

--- commands/command.go
+++ commands/command.go
@@ -3,18 +3,66 @@ package commands
 import (
 	"context"
 	"flag"
+	"fmt"
+	"strings"
 
 	"github.com/google/subcommands"
+
+	botLog "github.com/myyang/imbot/log"
 )
+
+var (
+	defaultCommandsCmd = &commandsCmd{}
+
+	customCommands = []subcommands.Command{
+		defaultCommandsCmd,
+	}
+)
+
+type commandsCmd struct {
+	usage string
+}
+
+func (c *commandsCmd) Name() string             { return "commands" }
+func (c *commandsCmd) Synopsis() string         { return "show available commands" }
+func (c *commandsCmd) SetFlags(f *flag.FlagSet) {}
+func (c *commandsCmd) Usage() string            { return c.Name() + ":\n    " + c.Synopsis() }
+
+func (c *commandsCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interface{}) subcommands.ExitStatus {
+	// add commands here for revealing
+	logger := ctx.Value(botLog.CtxLogger).(botLog.Logger)
+	if c.usage != "" {
+		logger.Infof("%s\n", c.usage)
+		return subcommands.ExitSuccess
+	}
+
+	b := strings.Builder{}
+	b.WriteString("available commands:\n\n")
+	for _, cc := range customCommands {
+		b.WriteString(fmt.Sprintf("%-30v%s\n", cc.Name(), cc.Synopsis()))
+	}
+
+	b.WriteString("\nOr use `CMD help` for more details\n")
+
+	c.usage = b.String()
+	logger.Infof("%s", c.usage)
+	return subcommands.ExitSuccess
+}
 
 func newCmd(ctx context.Context) (topLevelFS *flag.FlagSet, cmd *subcommands.Commander) {
 	topLevelFS = flag.NewFlagSet("top-level", flag.ContinueOnError)
 
 	cmd = subcommands.NewCommander(topLevelFS, "root")
+
+	// custom commands
+	for _, cc := range customCommands {
+		cmd.Register(cc, "op")
+	}
+
 	return
 }
 
-// Execute k
+// Execute is entrypoint for imbot commands.
 func Execute(
 	ctx context.Context,
 	args []string,
@@ -26,10 +74,14 @@ func Execute(
 
 	err = fs.Parse(args)
 	if err != nil {
+		defaultCommandsCmd.Execute(ctx, fs)
 		exitStatus = subcommands.ExitUsageError
 		return
 	}
 
 	exitStatus = cmd.Execute(ctx)
+	if exitStatus == subcommands.ExitUsageError {
+		defaultCommandsCmd.Execute(ctx, fs)
+	}
 	return
 }
