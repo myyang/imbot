7fffc21c71573a9c1cca2364c2ab87d5fc277bd5
Meng-Ying Yang <ymy1019@gmail.com>
2021-07-07 09:43:26 +0800

    commands: fix root command testcase

--- commands/command_test.go
+++ commands/command_test.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"os"
 	"testing"
+
+	botLog "github.com/myyang/imbot/log"
 )
 
 func TestCommand(t *testing.T) {
 	ctx := context.Background()
+	ctx = context.WithValue(ctx, botLog.CtxLogger, &botLog.DebugLogger{})
 
 	Execute(ctx, os.Args[1:])
 }
